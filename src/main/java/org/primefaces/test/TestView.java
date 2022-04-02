4ff07c0ec0cc9bb713c3335fb3395d80ec2391d8
Sandi Semrov <semrola007@gmail.com>
2022-04-02 12:05:24 +0200

    Lazy test

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -3,11 +3,20 @@ package org.primefaces.test;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import lombok.Data;
+import org.primefaces.PrimeFaces;
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
 
 @Data
 @Named
@@ -18,10 +27,43 @@ public class TestView implements Serializable {
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
-    
+    private LazyDataModel<String> model;
+
+    private List<String> list;
+
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+
+        list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(i + "");
+        }
+
+        model = new LazyDataModel<String>() {
+            @Override
+            public int count(Map<String, FilterMeta> map) {
+                return list.size();
+            }
+
+            @Override
+            public List<String> load(int first, int page, Map<String, SortMeta> map, Map<String, FilterMeta> map1) {
+                testMsgs();
+                PrimeFaces.current().ajax().update("msgs");
+                System.out.println("here");
+                return list.subList(first, first + page);
+            }
+        };
     }
+
+    public void testMsgs() {
+        FacesMessage msg = new FacesMessage("Test message");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public LazyDataModel<String> getModel() {
+        return model;
+    }
+
 
 }
